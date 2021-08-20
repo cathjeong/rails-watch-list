0606b9ae2eacc8185e271f323127952b0f3aeffa
Catherine Jeong <cathjeong@gmail.com>
2021-08-21 00:46:34 +0900

    working on heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# require 'faker'
+
+Movie.destroy_all
+
+# puts "Starting to run seeds file..."
+
+# 10.times do
+#   movie = Movie.create!(
+#     title: Faker::Movie.title,
+#     overview: Faker::Movie.quote,
+#     poster_url: Faker::Internet.url,
+#     rating: rand(1..10)
+#   )
+#   puts "Created movie with id #{movie.id}"
+# end
+
+require 'json'
+require 'open-uri'
+
+# url = 'http://tmdb.lewagon.com/movie/top_rated?api_key=ce9896efb804a19c567473d86dba682f'
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+# movie_serialized = Movie.open(url).read
+movies = JSON.parse(open(url).read)["results"]
+puts movies.count
+movies.each do |movie|
+  puts movie
+end
+
+# puts "#{movie['title']} - #{movie['overview']} - #{movie['poster_url']} - #{movie['rating']}"
